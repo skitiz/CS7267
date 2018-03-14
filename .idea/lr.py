ad529f341a83bd9f7d2dffac20b7dac42aad6451
Kshitij Bantupalli <kshitij.bantupalli@hotmail.com>
2018-03-14 09:18:49 -0400

    First commit

---
+++ .idea/lr.py
@@ -0,0 +1,14 @@
+from sklearn import datasets,linear_model
+from sklearn.model_selection import KFold
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+kf = kFold(len())
+
+// Perform k fold cross validation
+// For each value of k train logistic regression using gradient descent
+// Compute true positive rates and false positive rates for ROC curve
+// Plot ROC curve for 10-cross validation
+
+dataset =  pd.
