97eb601f681b7d579abc68aaa2c2e84860dac762
Kshitij Bantupalli <kshitij.bantupalli@gmail.com>
2018-03-15 13:16:08 -0400

    :children_crossing: :construction:

--- logistic-regression/logistic-regression.py
+++ logistic-regression/logistic-regression.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import KFold
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 def predict_class(training_data, w):
@@ -50,6 +51,7 @@ def split_data(learning_rate, threshold, gradient_steps):
     kf.get_n_splits(dataset)
     true_positive_rates = []
     false_positive_rates = []
+    total_costs = []
     for training, testing in kf.split(dataset):
         training_split = dataset[training]
         testing_split = dataset[testing]
@@ -58,11 +60,34 @@ def split_data(learning_rate, threshold, gradient_steps):
         testing_labels = np.array(testing_split[:, 0])
         testing_data = normalize(np.array(testing_split[:, 1:]))
         weights, costs = gradient_descent(training_data, training_labels, learning_rate, gradient_steps)
+        total_costs.append(costs)
         true_positive, false_positive = calculate_class(testing_data, testing_labels, weights, threshold)
         true_positive_rates.append(true_positive)
         false_positive_rates.append(false_positive)
         # print(training_labels.shape, training_data.shape, testing_labels.shape, testing_data.shape)
-    return true_positive_rates, false_positive_rates, costs
+    average_true = sum(true_positive_rates) / len(true_positive_rates)
+    average_false = sum(false_positive_rates) / len(false_positive_rates)
+    average_true += [0, 1]
+    average_false += [0, 1]
+    print("Average True Positive Rate : ", average_true)
+    print("Average False Positive Rate: ", average_false)
+    average_true.sort()
+    average_false.sort()
+    plt.figure(1)
+    plt.title("Gradient Descent Convergence, fold: 0")
+    plt.plot(costs[0])
+
+    plt.figure(2)
+    plt.title('Receiver Operating Characteristic')
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    # plt.plot([0,falsePositiveRate,1], [0,truePositiveRate,1])
+    plt.plot(false_positive_rates, true_positive_rates)
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    # return true_positive_rates, false_positive_rates, total_costs
 
 
 def normalize(x):
@@ -71,25 +96,15 @@ def normalize(x):
 
 dataset = np.genfromtxt('MNIST_CV.csv', delimiter=',', dtype=int, skip_header=1)
 
+start_time = time.time()
+
 
 def main():
     learning_rate = 1e-2
     threshold = 7
     gradient_steps = 1000
-    true_positive, false_positive, costs = split_data(learning_rate, threshold, gradient_steps)
-    average_true = sum(true_positive) / len(true_positive)
-    average_false = sum(false_positive) / len(false_positive)
-    average_true += [0, 1]
-    average_false += [0, 1]
-    average_true.sort()
-    average_false.sort()
-    # # plt.plot(costs[0])
-    # # plt.figure(2)
-    # plt.plot(average_true, average_false)
-    # plt.plot([0, 1],  [0, 1], 'r--')
-    # plt.show()
-    plt.plot(costs[0])
-    plt.show()
+    print("Logistic Regression on MNIST data. Classifying 6's and 8's.")
+    split_data(learning_rate, threshold, gradient_steps)
 
 
 if __name__ == '__main__':
